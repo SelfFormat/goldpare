82a76c41166f76e6eed82993cb4f41bc0653e1a9
Krzysztof <krzysztof@selfformat.com>
2020-11-30 00:24:28 +0100

    add id to item

--- shared/src/commonMain/kotlin/com/selfformat/goldpare/shared/model/GoldItem.kt
+++ shared/src/commonMain/kotlin/com/selfformat/goldpare/shared/model/GoldItem.kt
@@ -3,4 +3,4 @@ package com.selfformat.goldpare.shared.model
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class GoldItem(val price: String, val title: String, val link: String, val website: String)
+data class GoldItem(val id: Long, val price: String, val title: String, val link: String, val website: String)
