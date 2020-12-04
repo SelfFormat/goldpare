b7e6fd52bf7719fde73c145f7b5c493cdaf2a433
Veriqus <krzysztof@selfformat.com>
2020-12-05 00:39:08 +0100

    extract APIGOldItem to separate file

--- shared/src/commonMain/kotlin/com/selfformat/goldpare/shared/model/GoldItem.kt
+++ shared/src/commonMain/kotlin/com/selfformat/goldpare/shared/model/GoldItem.kt
@@ -1,8 +1,3 @@
 package com.selfformat.goldpare.shared.model
 
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class APIGoldItem(val price: String?, val title: String, val link: String, val website: String)
-
 data class GoldItem(val id: Long, val price: String?, val title: String, val link: String, val website: String)
