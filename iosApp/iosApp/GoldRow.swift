f0c05fbae5da1b2f0ba8689dbab01924b7298f1e
Veriqus <krzysztof@selfformat.com>
2020-12-04 22:39:43 +0100

    add default state for price if it will be null for iOS app

--- iosApp/iosApp/GoldRow.swift
+++ iosApp/iosApp/GoldRow.swift
@@ -7,8 +7,9 @@ struct GoldRow: View {
     var body: some View {
         HStack() {
             VStack(alignment: .leading, spacing: 8.0) {
+                let price = goldItem.price ?? "no price available"
                 Text("Title: \(goldItem.title)")
-                Text("Price: \(goldItem.price)")
+                Text("Price: \(price)")
                 Text("Link: \(goldItem.link)")
                 Text("Website: \(goldItem.website)")
             }
