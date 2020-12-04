627fb3b1c932cb0bba407cedf9b1996deb89c148
Veriqus <krzysztof@selfformat.com>
2020-12-04 23:51:03 +0100

    handling onclick for both platforms that leads to offer website

--- androidApp/src/main/java/com/selfformat/goldpare/androidApp/compose/MainComposeActivity.kt
+++ androidApp/src/main/java/com/selfformat/goldpare/androidApp/compose/MainComposeActivity.kt
@@ -1,22 +1,31 @@
 package com.selfformat.goldpare.androidApp.compose
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumnFor
 import androidx.compose.material.Card
-import androidx.compose.material.Text
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.AmbientContext
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.content.ContextCompat.startActivity
 import com.selfformat.goldpare.androidApp.compose.ui.GoldpareTheme
 import com.selfformat.goldpare.shared.GoldSDK
 import com.selfformat.goldpare.shared.cache.DatabaseDriverFactory
@@ -54,29 +63,37 @@ class MainComposeActivity : AppCompatActivity() {
 
 @Composable
 fun LazyGoldColumn(
-        goldItems: List<GoldItem>,
+        goldItems: List<GoldItem>
 ) {
     Surface(modifier = Modifier.fillMaxSize()) {
+        val context = AmbientContext.current
         LazyColumnFor(goldItems) { item ->
-            GoldRow(item)
+            GoldRow(item, onClick = { openWebPage(item.link, context = context) })
         }
     }
 }
 
 
 @Composable
-fun GoldRow(item: GoldItem) {
+fun GoldRow(item: GoldItem, onClick: (() -> Unit)) {
     Card(Modifier
-            .padding(8.dp)
-            .fillMaxWidth()) {
+        .padding(8.dp)
+        .fillMaxWidth()
+        .clickable(onClick = onClick)
+    ) {
         Column(Modifier
                 .padding(16.dp)) {
             Text(text = item.title)
             Text(text = item.price.orEmpty())
-            Text(text = item.link)
-            Text(text = item.website)
+            Text(text = item.website, modifier = Modifier.align(Alignment.End))
         }
     }
+}
+
+fun openWebPage(url: String, context: Context) {
+    val webpage: Uri = Uri.parse(url)
+    val intent = Intent(Intent.ACTION_VIEW, webpage)
+    startActivity(context, intent, null)
 }
 
 @Preview(showBackground = true)
@@ -84,6 +101,6 @@ fun GoldRow(item: GoldItem) {
 fun DefaultPreview() {
     val goldItem = GoldItem(1, "Gold 1oz", "3000zł", "www.gold.com/1oz", "gold.com")
     GoldpareTheme {
-        GoldRow(goldItem)
+        GoldRow(goldItem) { }
     }
 }
--- iosApp/iosApp/GoldRow.swift
+++ iosApp/iosApp/GoldRow.swift
@@ -2,16 +2,22 @@ import SwiftUI
 import shared
 
 struct GoldRow: View {
+    
     var goldItem: GoldItem
     
     var body: some View {
-        HStack() {
-            VStack(alignment: .leading, spacing: 8.0) {
-                let price = goldItem.price ?? "no price available"
-                Text("Title: \(goldItem.title)")
-                Text("Price: \(price)")
-                Text("Link: \(goldItem.link)")
-                Text("Website: \(goldItem.website)")
+        Button(action: {
+            if let url = URL(string: goldItem.link) { UIApplication.shared.open(url) }
+        }) {
+            HStack() {
+                VStack(alignment: .trailing){
+                    Text(goldItem.website)
+                }
+                VStack(alignment: .leading, spacing: 8.0) {
+                    let price = goldItem.price ?? "no price available"
+                    Text(goldItem.title).bold()
+                    Text(price)
+                }
             }
             Spacer()
         }
