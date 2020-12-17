8a42dd9c829cb8abd93cef5a6c9e3c728275d685
Veriqus <krzysztof@selfformat.com>
2020-12-17 22:02:37 +0100

    add home fragment

---
+++ androidApp/src/main/java/com/selfformat/goldpare/androidApp/compose/HomeFragment.kt
@@ -0,0 +1,30 @@
+package com.selfformat.goldpare.androidApp.compose
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.platform.ComposeView
+import androidx.fragment.app.Fragment
+import com.selfformat.goldpare.androidApp.compose.ui.GoldpareTheme
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return ComposeView(requireContext()).apply {
+            setContent {
+                GoldpareTheme {
+                    Surface(color = MaterialTheme.colors.background) {
+                        LazyGoldColumn()
+                    }
+                }
+            }
+        }
+    }
+}
