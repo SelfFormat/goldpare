9e1dc08569366be10bb2a3d790217f41f9d1f289
Veriqus <krzysztof@selfformat.com>
2020-12-18 20:24:05 +0100

    add more coin types

--- androidApp/src/main/java/com/selfformat/goldpare/androidApp/compose/GoldCoinType.kt
+++ androidApp/src/main/java/com/selfformat/goldpare/androidApp/compose/GoldCoinType.kt
@@ -1,7 +1,15 @@
 package com.selfformat.goldpare.androidApp.compose
 
 enum class GoldCoinType(coinName: String = "") {
+    //TODO: Change coinName to be regexp based on results
     KRUGERRAND("Krugerrand"),
     DUKAT("Dukat"),
+    BRITANNIA("Britannia"),
+    LISC_KLONOWY("Klonowy"),
+    AUSTRALIJSKI_KANGUR("Kangur"),
+    CHINSKA_PANDA("Panda"),
+    AMERYKANSKI_BIZON("Bizon"),
+    AMERYKANSKI_ORZEŁ("Orzeł"),
+    FILCHARMONICY_WIEDENSCY("Filharmonik"),
     ALL()
 }
