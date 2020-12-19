69d566fa4173613e8dac301cc01005534cb5ee97
Veriqus <krzysztof@selfformat.com>
2020-12-19 11:52:46 +0100

    change endpoint as new contains more items

--- shared/src/commonMain/kotlin/com/selfformat/goldpare/shared/api/GoldApi.kt
+++ shared/src/commonMain/kotlin/com/selfformat/goldpare/shared/api/GoldApi.kt
@@ -21,6 +21,6 @@ class GoldApi {
     }
 
     companion object {
-        private val GOLD_ENDPOINT = Url("https://selfformat.com/coin_bar.json")
+        private val GOLD_ENDPOINT = Url("https://selfformat.com/coin_new_mints.json")
     }
 }
