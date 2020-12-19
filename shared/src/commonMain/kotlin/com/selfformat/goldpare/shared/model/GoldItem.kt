b3f9e6e3cd2a135b2b9ec295b5b3f9e03f9446f3
Veriqus <krzysztof@selfformat.com>
2020-12-19 12:37:26 +0100

    add mint enum

--- shared/src/commonMain/kotlin/com/selfformat/goldpare/shared/model/GoldItem.kt
+++ shared/src/commonMain/kotlin/com/selfformat/goldpare/shared/model/GoldItem.kt
@@ -25,31 +25,14 @@ data class GoldItem(
 
     val pricePerOunce: Double? = pricePerGram?.times(OZ_TROY)
 
-    val mintFullName: String =
-        when (website) {
-            "goldenmark" -> "Goldenmark"
-            "79element" -> "79th element"
-            "mennicacompl" -> "Mennica Polska"
-            "mennicakapitalowa" -> "Mennica Kapitałowa"
-            "mennicakrajowa" -> "Mennica Krajowa"
-            "mennicamazovia" -> "Mennica Mazovia"
-            "metalelokacyjne" -> "Metale Lokacyjne"
-            "metalmarketu" -> "Metal Market Europe"
-            "wyrobymennicze" -> "Wyroby Mennicze"
-            "mennicaskarbowa" -> "Mennica Skarbowa"
-            "coininvest" -> "Coininvest"
-            "travex" -> "Travex"
-            "mennica24" -> "Mennica24"
-            "zlotauncja" -> "Złota Uncja"
-            "mennicakrakowska" -> "Mennica Krakowska"
-            "mennicagdanska" -> "Mennica Gdańska"
-            "mennicazielona" -> "Mennica Zielona"
-            "goldco" -> "Goldco"
-            "idfmetale" -> "IDF metale"
-            "chojnackiikwiecien" -> "Chojnacki i Kwiecień"
-            "flyingatomgold" -> "Flyingatom"
-            else -> "Sklep"
+    fun mintFullName(): String {
+        Mint.values().forEach {
+            if (website == it.name) {
+                return it.fullName
+            }
         }
+        return "Uknown provider"
+    }
 
     fun weightInGrams(weight: String?): Double? {
         val weightWithoutWhitespace =
