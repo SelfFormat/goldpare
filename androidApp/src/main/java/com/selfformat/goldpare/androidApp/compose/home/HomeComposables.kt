9ebc370cadebf42e35dda55bf101d522f350dfdc
Veriqus <krzysztof@selfformat.com>
2020-12-23 17:00:43 +0100

    fix categories to expand size equally with use of RowScope

--- androidApp/src/main/java/com/selfformat/goldpare/androidApp/compose/home/HomeComposables.kt
+++ androidApp/src/main/java/com/selfformat/goldpare/androidApp/compose/home/HomeComposables.kt
@@ -8,11 +8,13 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.defaultMinSizeConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.preferredWidth
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -202,16 +204,20 @@ private fun Categories(viewModel: HomeViewModel) {
         horizontalArrangement = Arrangement.SpaceAround,
         modifier = Modifier.fillMaxWidth().padding(bottom = dp16)
     ) {
-        CategoryBox("ALL") { viewModel.search("All") }
-        CategoryBox("MONETY") { viewModel.search("Monety") }
-        CategoryBox("SZTABKI") { viewModel.search("Sztabki") }
+        Spacer(modifier = Modifier.preferredWidth(dp16))
+        CategoryBox(modifier = Modifier.weight(1f), text = "ALL") { viewModel.search("All") }
+        Spacer(modifier = Modifier.preferredWidth(dp8))
+        CategoryBox(modifier = Modifier.weight(1f), text = "MONETY") { viewModel.search("Monety") }
+        Spacer(modifier = Modifier.preferredWidth(dp8))
+        CategoryBox(modifier = Modifier.weight(1f), text = "SZTABKI") { viewModel.search("Sztabki") }
+        Spacer(modifier = Modifier.preferredWidth(dp16))
     }
 }
 
 @Composable
-private fun CategoryBox(text: String, onClick: (() -> Unit)) {
-    Box(
-        Modifier.background(
+private fun CategoryBox(text: String, modifier: Modifier, onClick: (() -> Unit)) {
+    Column(
+        modifier.background(
             brush = Brush.verticalGradient(
                 0.0f to categoryGradientTop,
                 1.0f to categoryGradientBottom // TODO add another color set for darkmode
@@ -219,7 +225,7 @@ private fun CategoryBox(text: String, onClick: (() -> Unit)) {
             RoundedCornerShape(dp4),
         ).clickable(onClick = onClick).defaultMinSizeConstraints(categoryBoxMinSize)
     ) {
-        Text(text = text, Modifier.padding(dp20).wrapContentSize().align(Alignment.BottomCenter))
+        Text(text = text, Modifier.padding(dp20).wrapContentSize().align(Alignment.CenterHorizontally))
     }
 }
 
--- androidApp/src/main/java/com/selfformat/goldpare/androidApp/compose/results/ResultComposables.kt
+++ androidApp/src/main/java/com/selfformat/goldpare/androidApp/compose/results/ResultComposables.kt
@@ -2,13 +2,17 @@ package com.selfformat.goldpare.androidApp.compose.results
 
 import android.widget.Toast
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -17,7 +21,9 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.AmbientContext
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.viewinterop.viewModel
+import com.selfformat.goldpare.androidApp.R
 import com.selfformat.goldpare.androidApp.compose.XauPlnViewModel
 import com.selfformat.goldpare.androidApp.compose.commonComposables.BottomGradient
 import com.selfformat.goldpare.androidApp.compose.commonComposables.ErrorView
@@ -25,6 +31,8 @@ import com.selfformat.goldpare.androidApp.compose.commonComposables.GoldCard
 import com.selfformat.goldpare.androidApp.compose.commonComposables.Loading
 import com.selfformat.goldpare.androidApp.compose.commonComposables.ResultsSearchView
 import com.selfformat.goldpare.androidApp.compose.home.HomeViewModel
+import com.selfformat.goldpare.androidApp.compose.theme.dp8
+import com.selfformat.goldpare.androidApp.compose.theme.noElevation
 import com.selfformat.goldpare.androidApp.compose.util.openWebPage
 import com.selfformat.goldpare.shared.api.XauPln
 import com.selfformat.goldpare.shared.model.GoldItem
@@ -44,14 +52,13 @@ fun ResultsView() {
         xauPln.let { xau_to_pln ->
             if (xauPln != null) {
                 Column {
-                    SortFilterCTA()
-                    ListOfAppliedFilters()
                     state.let {
                         when (it) {
                             is ResultViewModel.State.Loaded -> {
                                 GoldResults(
                                     list = it.goldItems,
                                     xauPln = xau_to_pln!!,
+                                    resultViewModel = viewModel,
                                     title = it.title
                                 )
                                 BottomGradient()
@@ -73,11 +80,18 @@ fun ResultsView() {
 @ExperimentalFoundationApi
 @ExperimentalUnsignedTypes
 @Composable
-private fun GoldResults(list: List<GoldItem>, xauPln: XauPln, title: String = "Złoto") {
+private fun GoldResults(
+    list: List<GoldItem>,
+    xauPln: XauPln,
+    resultViewModel: ResultViewModel,
+    title: String = "Złoto"
+) {
     val context = AmbientContext.current
     LazyColumn {
         item {
             TopBar(title)
+            SortFilterCTA(resultViewModel)
+            ListOfAppliedFilters()
         }
         items(list) {
             GoldCard(it, xauPln) {
@@ -96,11 +110,13 @@ private fun TopBar(title: String) {
     val context = AmbientContext.current
     val homeViewModel = viewModel<HomeViewModel>()
     TopAppBar(
-        title = { ResultsSearchView(
-            viewModel = homeViewModel,
-            function = { Toast.makeText(context, "not yet implemented", Toast.LENGTH_LONG).show() },
-            placeholderText = title
-        ) },
+        title = {
+            ResultsSearchView(
+                viewModel = homeViewModel,
+                function = { Toast.makeText(context, "not yet implemented", Toast.LENGTH_LONG).show() },
+                placeholderText = title
+            )
+        },
         backgroundColor = Color.White,
         navigationIcon = {
             IconButton(
@@ -109,13 +125,21 @@ private fun TopBar(title: String) {
                     Icon(Icons.Filled.ArrowBack)
                 }
             )
-        }
+        },
+        elevation = noElevation
     )
 }
 
 @Composable
-private fun SortFilterCTA() {
-    Row {}
+private fun SortFilterCTA(viewModel: ResultViewModel) {
+
+    OutlinedButton(onClick = { viewModel.goToFiltersScreen() }, border = null) {
+        Image(
+            vectorResource(id = R.drawable.ic_filter_sort),
+            modifier = Modifier.padding(end = dp8)
+        )
+        Text(text = "FILTRUJ / SORTUJ", color = Color.Black)
+    }
 }
 
 @Composable
